d46f148a5ed85926999f2a9ba746bf8ef832272b
Lachlan Williams <lwil0039@student.monash.edu>
2024-08-26 12:20:50 +1000

    Research page displays research card

---
+++ DementiaResearchWebsite/src/components/ResearchCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Card from 'primevue/card';
+import { defineProps } from 'vue';
+
+// Define props to accept `title` and `content`
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+    <div class="card-container">
+        <Card class="custom-card">
+            <template #title>{{ title }}</template>
+            <template #content>
+                <div>
+                    <p>Publisher: {{ publisher }}</p>
+                    <span>Date: {{ date }}</span>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<style scoped>
+.custom-card {
+  background-color: #f5f5f5;
+  color: black;
+
+}
+.card-container {
+  padding-bottom: 1rem; /* Adjust padding as needed */
+}
+</style>
--- DementiaResearchWebsite/src/views/ResearchView.vue
+++ DementiaResearchWebsite/src/views/ResearchView.vue
@@ -1,13 +1,26 @@
+<script setup>
+// No script needed for now
+import research from "../assets/JSON/research.json";
+import ResearchCard from "@/components/ResearchCard.vue";
+</script>
+
 <template>
-    <div class="about">
-      <h1>Reearch Page</h1>
-      <p>This is our research page</p>
-    </div>
-  </template>
-  
-  <script setup>
-  // No script needed for now
-  </script>
-  
-  <style>
-  </style>
+  <h1>Reearch Page</h1>
+  <p>This is our research page</p>
+  <div>
+    <ResearchCard
+      v-for="researchitem in research"
+      :key="researchitem.title"
+      :title="researchitem.title"
+      :publisher="researchitem.publisher"
+      :date="researchitem.date"
+      class="clickable-card"
+    />
+  </div>
+</template>
+
+<style scoped>
+.clickable-card {
+  cursor: pointer;
+}
+</style>
